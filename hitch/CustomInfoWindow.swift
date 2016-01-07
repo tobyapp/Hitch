42644afc466d767023d34734ea7329e2cb24bc74
Toby Applegate <up661724@myport.ac.uk>
2016-01-07 16:27:24 +0000

    added custome message box when user clicks place marker

---
+++ hitch/CustomInfoWindow.swift
@@ -0,0 +1,23 @@
+//
+//  CustomInfoWindow.swift
+//  hitch
+//
+//  Created by Toby Applegate on 07/01/2016.
+//  Copyright © 2016 Toby Applegate. All rights reserved.
+//
+
+import UIKit
+
+class CustomInfoWindow: UIView {
+
+    @IBOutlet var label: UILabel!
+    
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
